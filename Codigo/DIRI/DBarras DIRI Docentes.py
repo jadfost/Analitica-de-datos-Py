b2b50fa5e53480529ef300dccfed16381431e4b9
nirttson <stevenorduz@hotmail.com>
2023-07-30 18:37:36 -0500

    V1.2.6 - PRUEBA FINAL PARA DOCENTES DIRI

--- Codigo/DIRI/DBarras DIRI Docentes.py
+++ Codigo/DIRI/DBarras DIRI Docentes.py
@@ -2,32 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Cargamos los datos desde el archivo CSV en un DataFrame
-df_docentes = pd.read_csv('Datos/DIRI/DIRI Mov. Docente.csv')
+df_docentes = pd.read_csv('Datos/DIRI/DIRI Mov. Docente.csv', dtype={'PE  Periodo movilidad': float, 'PS Periodo movilidad': float})
 
-# Filtrar el DataFrame para obtener solo los datos con movilidades en "PE" y "PS"
-df_movilidad_pe_ps = df_docentes[(df_docentes['PE - Docente Nacional'].notnull()) & (df_docentes['PS - Docente Nacional'].notnull())]
-
-# Ordenar los datos por la columna 'PE - Docente Nacional' de manera descendente para PE
-df_pe_nacional = df_movilidad_pe_ps.sort_values(by='PE - Docente Nacional', ascending=False)
+# Contar la frecuencia de aparición de cada docente en PE
+frecuencia_pe = df_docentes['PE - Docente Nacional'].value_counts()
 
 # Tomar los tres docentes con más movimientos en PE
-top_docentes_pe_nacional = df_pe_nacional.head(3)
+top_docentes_pe_nacional = frecuencia_pe.head(3)
 
-# Ordenar los datos por la columna 'PS - Docente Nacional' de manera descendente para PS
-df_ps_nacional = df_movilidad_pe_ps.sort_values(by='PS - Docente Nacional', ascending=False)
+# Contar la frecuencia de aparición de cada docente en PS
+frecuencia_ps = df_docentes['PS - Docente Nacional'].value_counts()
 
 # Tomar los tres docentes con más movimientos en PS
-top_docentes_ps_nacional = df_ps_nacional.head(3)
+top_docentes_ps_nacional = frecuencia_ps.head(3)
 
 # Lista de nombres de los docentes con más movimientos en PE - Docente Nacional
-nombres_docentes_pe = top_docentes_pe_nacional['PE - Docente Nacional'].tolist()
-# Lista de valores de movilidades para los docentes con más movimientos en PE - Docente Nacional
-movilidades_docentes_pe = top_docentes_pe_nacional['PE  Periodo movilidad'].tolist()
+nombres_docentes_pe = top_docentes_pe_nacional.index.tolist()
+# Lista de valores de movimientos para los docentes con más movimientos en PE - Docente Nacional
+movilidades_docentes_pe = top_docentes_pe_nacional.tolist()
 
 # Lista de nombres de los docentes con más movimientos en PS - Docente Nacional
-nombres_docentes_ps = top_docentes_ps_nacional['PS - Docente Nacional'].tolist()
-# Lista de valores de movilidades para los docentes con más movimientos en PS - Docente Nacional
-movilidades_docentes_ps = top_docentes_ps_nacional['PS Periodo movilidad'].tolist()
+nombres_docentes_ps = top_docentes_ps_nacional.index.tolist()
+# Lista de valores de movimientos para los docentes con más movimientos en PS - Docente Nacional
+movilidades_docentes_ps = top_docentes_ps_nacional.tolist()
 
 # Crear el gráfico de barras para PE
 plt.figure(figsize=(15, 6))
