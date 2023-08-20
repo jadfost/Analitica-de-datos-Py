44d54d20269f87fc496fc7cc7ccb24d0d46625bb
Jared Foster <stevenorduz@hotmail.com>
2023-08-19 19:12:02 -0500

    V1.3.7  - DIRI done

--- Codigo/DIRI/DGrafico DIRI.py
+++ Codigo/DIRI/DGrafico DIRI.py
@@ -8,13 +8,13 @@ import plotly.express as px
 df_docentes = pd.read_csv('Datos/DIRI/DIRI Mov. Docente.csv')
 df_estudiantes = pd.read_csv('Datos/DIRI/DIRI Mov.estudiantil.csv')
 
-
 # Crear una aplicación Dash
 app = dash.Dash(__name__)
 
 # Definir el diseño de la aplicación
 app.layout = html.Div([
     html.H1('Visualización de Datos DIRI'),
+    html.P('Selecciona una tabla:'),
     dcc.Dropdown(
         id='tabla-dropdown',
         options=[
@@ -24,49 +24,55 @@ app.layout = html.Div([
         value='docentes',  # Valor inicial
         multi=False
     ),
+    html.P('Selecciona una variable de análisis:'),
+    dcc.Dropdown(
+        id='campo-dropdown',
+        multi=False
+    ),
     dcc.Graph(id='grafico-circular')
 ])
+
+# Crear una función para actualizar las opciones del menú desplegable de campos
+@app.callback(
+    Output('campo-dropdown', 'options'),
+    Input('tabla-dropdown', 'value')
+)
+def actualizar_campos_disponibles(tabla_seleccionada):
+    if tabla_seleccionada == 'docentes':
+        columnas_disponibles = df_docentes.columns
+    elif tabla_seleccionada == 'estudiantes':
+        columnas_disponibles = df_estudiantes.columns
+    else:
+        columnas_disponibles = []
+
+    opciones = [{'label': columna, 'value': columna} for columna in columnas_disponibles]
+    return opciones
 
 # Crear una función para actualizar el gráfico circular
 @app.callback(
     Output('grafico-circular', 'figure'),
-    Input('tabla-dropdown', 'value')
+    Input('tabla-dropdown', 'value'),
+    Input('campo-dropdown', 'value')
 )
-def actualizar_grafico(tabla_seleccionada):
+def actualizar_grafico(tabla_seleccionada, campo_seleccionado):
     if tabla_seleccionada == 'docentes':
         data = df_docentes
-        titulo = 'Docentes con Mayor Población'
-        columna_entrante_nacional = 'PE - Docente Nacional'
-        columna_entrante_internacional = 'PE - Docente Internacional'
-        columna_saliente_nacional = 'PS - Docente Nacional'
-        columna_saliente_internacional = 'PS - Docente Internacional'
+        titulo = 'Docentes'
     elif tabla_seleccionada == 'estudiantes':
         data = df_estudiantes
-        titulo = 'Estudiantes con Mayor Población'
-        columna_entrante_nacional = 'PE - Estudiantes Nacional'
-        columna_entrante_internacional = 'PE - Estudiantes Internacional'
-        columna_saliente_nacional = 'PS - Estudiantes Nacional'
-        columna_saliente_internacional = 'PS - Estudiantes Internacional'
+        titulo = 'Estudiantes'
     else:
         data = pd.DataFrame()
         titulo = ''
 
-    if not data.empty:
-        # Combinar las columnas de población entrante y saliente en una sola columna
-        data['Poblacion'] = data[columna_entrante_nacional] + data[columna_entrante_internacional] + data[columna_saliente_nacional] + data[columna_saliente_internacional]
-        
-        # Ordenar los datos por población en orden descendente
-        data_ordenada = data.sort_values(by=['Poblacion'], ascending=False)
-        
-        # Tomar los 5 primeros registros con mayor población
-        data_top_5 = data_ordenada.head(5)
-        
-        # Crear un gráfico circular para mostrar los nombres de las personas
-        fig = px.pie(data_top_5, names='Poblacion', 
-                     title=f'{titulo} - Población Entrante y Saliente', 
-                     labels={'Poblacion': 'Nombre'})
+    # Filtrar filas con campos vacíos en la columna seleccionada
+    if not data.empty and campo_seleccionado in data.columns:
+        data = data.dropna(subset=[campo_seleccionado])
+
+    if not data.empty and campo_seleccionado in data.columns:
+        fig = px.pie(data, names=campo_seleccionado, title=f'{titulo} - {campo_seleccionado}')
     else:
-        fig = px.pie()  # Gráfico vacío si no se ha seleccionado una tabla
+        fig = px.pie()  # Gráfico circular vacío si no se ha seleccionado una tabla o campo válido
 
     return fig
 
