274ec8145b5632fa54d886fc2226f31a8f52ea20
jafost <stevenorduz@hotmail.com>
2023-10-01 19:37:24 -0500

    V6.7 OCME

--- Codigo/OCME/DBarras OCME.py
+++ Codigo/OCME/DBarras OCME.py
@@ -4,7 +4,6 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-
 # Leer el archivo CSV con los datos de OCME
 data_ocme = pd.read_csv('Datos/OCME/OCME 10-18.csv')
 
@@ -14,28 +13,46 @@ app = dash.Dash(__name__)
 # Definir el diseño de la aplicación
 app.layout = html.Div([
     html.H1('Visualización de Datos OCME'),
-    html.P("Selecciona una variable de analisis:"),
+    html.P("Selecciona una variable de análisis:"),
     dcc.Dropdown(
         id='campo-dropdown',
         options=[{'label': columna, 'value': columna} for columna in data_ocme.columns],
         value='AÑO/PERIODO',  # Cambiado a 'AÑO/PERIODO'
         multi=False
     ),
-    dcc.Graph(id='grafico-barras')
+    dcc.Graph(id='grafico-barras'),
+    html.Div(id='descripcion-grafico')  # Aquí mostraremos la descripción del gráfico
 ])
 
-# Crear una función para actualizar el gráfico de barras
+# Función para obtener una descripción del campo seleccionado
+def obtener_descripcion(campo_seleccionado):
+    # Puedes agregar aquí descripciones para cada campo seleccionado
+    descripciones = {
+    'AÑO/PERIODO': 'Esta gráfica muestra la evolución a lo largo de los años/periodos.',
+    'NOMBRE DE LOS MEDIOS DE DIVULGACIÓN': 'Esta gráfica muestra el nombre de los medios de divulgación y la cantidad de veces utilizados.',
+    'LUGAR DE DIVULGACIÓN': 'Esta gráfica muestra el lugar de divulgación de la información y la cantidad de veces que se realizo.',
+    'PROPÓSITO DEL MEDIO UTILIZADO': 'Esta gráfica muestra el propósito del medio y la cantidad de veces utilizado.',
+    'NÚMERO APROX. DE PERSONAS INFORMADAS': 'Esta gráfica muestra el número aproximado de personas informadas.'
+}
+    return descripciones.get(campo_seleccionado, 'Descripción no disponible para este campo.')
+
+# Crear una función para actualizar el gráfico de barras y la descripción
 @app.callback(
-    Output('grafico-barras', 'figure'),
+    [Output('grafico-barras', 'figure'),
+     Output('descripcion-grafico', 'children')],
     Input('campo-dropdown', 'value')
 )
 def actualizar_grafico(campo_seleccionado):
     if campo_seleccionado is not None:
         fig = px.bar(data_ocme, x=campo_seleccionado, title=f'Gráfico de barras para {campo_seleccionado}')
+        descripcion = obtener_descripcion(campo_seleccionado)
+        # Ajustar la orientación de las etiquetas en el eje x
+        fig.update_xaxes(tickangle=45)  # Cambia el ángulo a 45 grados para mejorar la legibilidad
     else:
         fig = px.bar()  # Gráfico vacío si no se ha seleccionado un campo
+        descripcion = 'Selecciona una variable para mostrar su gráfico y descripción.'
 
-    return fig
+    return fig, descripcion
 
 # Iniciar la aplicación
 if __name__ == '__main__':
--- Codigo/OCME/DGrafico OCME.py
+++ Codigo/OCME/DGrafico OCME.py
@@ -20,21 +20,56 @@ app.layout = html.Div([
         value='AÑO/PERIODO',  # Cambiado a 'AÑO/PERIODO'
         multi=False
     ),
-    dcc.Graph(id='grafico-circular')
+    dcc.Graph(id='grafico-circular'),
+    html.Div(id='descripcion-grafico')  # Aquí mostraremos la descripción del gráfico
 ])
 
-# Crear una función para actualizar el gráfico circular
+# Función para limitar la longitud de los textos en las leyendas
+def limitar_longitud_textos(textos, longitud_maxima=30):
+    return [texto[:longitud_maxima] + ('...' if len(texto) > longitud_maxima else '') for texto in textos]
+
+# Función para limitar la cantidad de categorías mostradas y agrupar otras en "Otros"
+def limitar_categorias(data, campo_seleccionado, max_categorias=10):
+    conteo_valores = data[campo_seleccionado].value_counts()
+    etiquetas = conteo_valores.index.tolist()
+    valores = conteo_valores.tolist()
+    
+    if len(etiquetas) > max_categorias:
+        etiquetas = etiquetas[:max_categorias]
+        valores = valores[:max_categorias]
+        etiquetas.append('Otros')
+        valores.append(sum(conteo_valores[max_categorias:]))
+    
+    return etiquetas, valores
+
+# Función para obtener una descripción del campo seleccionado
+def obtener_descripcion(campo_seleccionado):
+    descripciones = {
+        'AÑO/PERIODO': 'Este gráfico circular muestra la distribución de los datos a lo largo de diferentes años o períodos.',
+        'NOMBRE DE LOS MEDIOS DE DIVULGACIÓN': 'Este gráfico muestra cómo se distribuyen los datos entre los diferentes medios de divulgación utilizados.',
+        'LUGAR DE DIVULGACIÓN': 'Este gráfico muestra la distribución de los datos según los lugares de divulgación.',
+        'PROPÓSITO DEL MEDIO UTILIZADO': 'Este gráfico representa la distribución de datos en función del propósito del medio utilizado.',
+        'NÚMERO APROX. DE PERSONAS INFORMADAS': 'Este gráfico muestra cómo se distribuye el número aproximado de personas informadas.'
+    }
+    return descripciones.get(campo_seleccionado, 'Descripción no disponible para este campo.')
+
+# Crear una función para actualizar el gráfico circular y su descripción
 @app.callback(
-    Output('grafico-circular', 'figure'),
+    [Output('grafico-circular', 'figure'),
+     Output('descripcion-grafico', 'children')],
     Input('campo-dropdown', 'value')
 )
 def actualizar_grafico(campo_seleccionado):
     if campo_seleccionado is not None:
-        fig = px.pie(data_ocme, names=campo_seleccionado, title=f'Distribución de {campo_seleccionado}')
+        etiquetas, valores = limitar_categorias(data_ocme, campo_seleccionado)
+        etiquetas = limitar_longitud_textos(etiquetas)  # Limitar longitud de las leyendas
+        fig = px.pie(names=etiquetas, values=valores, title=f'Distribución de {campo_seleccionado}')
+        descripcion = obtener_descripcion(campo_seleccionado)
     else:
         fig = px.pie()  # Gráfico vacío si no se ha seleccionado un campo
+        descripcion = 'Selecciona una variable para mostrar su gráfico y descripción.'
 
-    return fig
+    return fig, html.P(descripcion)  # Mostrar la descripción debajo del gráfico
 
 # Iniciar la aplicación
 if __name__ == '__main__':
